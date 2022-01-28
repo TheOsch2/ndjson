1ea1e22aefe72da8d929184ff68713b43a4e5809
Vyacheslav V Sokolov <deathless@t-sk.ru>
2022-01-28 18:11:47 +0700

    Working...

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const util = require('util');
+const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 5000;
 
-express().get('/', (req, res) => {
+express().get('/', async (req, res) => {
   try {
     if (!req.query.url) {
       throw new Error('Correct call: "...?url=<url to fetch and convert>"');
     }
-    res.writeHead(200, 'Ok');
-    res.end(util.inspect(req.query.url));
+    res.writeHead(200, 'Ok', {
+      'Content-Type': 'application/x-ndjson'
+    });
+    // A temporary solution. The working one will load items one by one
+    // to achieve a normal performance on ...s of records 
+    const url = res.query.url;
+    const incoming = await fetch(req.query.url).json();
+    // Test
+    res.end(JSON.stringify(incoming, null, 2));
   } catch (err) {
     console.log(err);
     res.writeHead(500, 'Server error', {
