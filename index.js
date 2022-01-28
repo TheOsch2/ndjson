07364385c2ade98f2e00591d6e1855d536313032
Vyacheslav V Sokolov <deathless@t-sk.ru>
2022-01-28 17:47:31 +0700

    Working...

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const PORT = process.env.PORT || 5000;
 
 express().get('/', (req, res) => {
   try {
-    // Test yet
+    if (!res.query.url) {
+      throw new Error('Correct call: "...?url=<url to fetch and convert>"');
+    }
     res.writeHead(200, 'Ok');
     res.end(util.inspect(req.query));
   } catch (err) {
     console.log(err);
-    res.writeHead(500, 'Server error');
+    res.writeHead(500, 'Server error', {
+      'Content-Type': 'application/json'
+    });
     res.end(util.inspect(err));
   }
 }).listen(PORT);
