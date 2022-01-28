92ba22bb59137b1c62c365f4afe77462b56085a7
Vyacheslav V Sokolov <deathless@t-sk.ru>
2022-01-28 18:38:49 +0700

    Working...

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ express().get('/', async (req, res) => {
       throw new Error('Correct call: "...?url=<url to fetch and convert>"');
     }
     res.writeHead(200, 'Ok', {
-      'Content-Type': 'application/x-ndjson'
+      'Content-Type': 'application/json' // 'application/x-ndjson'
     });
     // A temporary solution. The working one will load items one by one
     // to achieve a normal performance on ...s of records 
     const url = res.query.url;
     const incoming = await fetch(url).json();
     // Test
-    res.end(JSON.stringify(incoming, null, 2));
+    res.end(incoming);
   } catch (err) {
     console.log(err);
     res.writeHead(500, 'Server error', {
