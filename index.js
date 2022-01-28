c447dc6129c5985087dab7b1b579907a5ee3447c
Vyacheslav V Sokolov <deathless@t-sk.ru>
2022-01-28 17:56:36 +0700

    Working...

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ express().get('/', (req, res) => {
       throw new Error('Correct call: "...?url=<url to fetch and convert>"');
     }
     res.writeHead(200, 'Ok');
-    res.end(util.inspect(req.query));
+    res.end(util.inspect(req.query.url));
   } catch (err) {
     console.log(err);
     res.writeHead(500, 'Server error', {
